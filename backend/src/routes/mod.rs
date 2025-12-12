eb528808a2efb992a8947cbc2ee4fc500a11fb3f
Pascal Alonso de Prado <pascal.alonso.de.prado@rwth-aachen.de>
2025-12-12 15:49:35 +0100

    changes to allow the prod build to work

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -6,7 +6,13 @@ pub mod v1;
 
 pub fn create_routes() -> Router {
     let cors = CorsLayer::new()
-        .allow_origin(HeaderValue::from_static("http://localhost:5173")) // frontend origin
+        .allow_origin(
+            [
+                "http://localhost:5173".parse::<HeaderValue>().unwrap(),
+                "http://localhost:4173".parse::<HeaderValue>().unwrap(),
+            ]
+            .to_vec(),
+        ) // frontend origins
         .allow_methods([Method::GET, Method::POST, Method::DELETE, Method::PUT])
         .allow_headers(Any);
 
