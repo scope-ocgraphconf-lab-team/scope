fa65da456912eb8c329af8063aabdf0619b3e9aa
Ipek Nur Ozcan <ipeknurozcan06@gmail.com>
2026-04-22 18:49:14 +0200

    excluding special activities from all event types

--- backend/src/handlers/resource_miner.rs
+++ backend/src/handlers/resource_miner.rs
@@ -15,7 +15,7 @@ pub struct ObjectNotResourceArc {
 pub struct ResourceMinerResponse {
     pub object_type_not_resource: Vec<String>,
     pub object_resource: Vec<String>,
-    pub all_event_types: Vec<String>,
+    pub non_special_event_types: Vec<String>,
     pub event_types_without_object_resource: Vec<String>,
     pub object_not_resource_arcs: Vec<ObjectNotResourceArc>,
     pub special_activity: Vec<String>,
@@ -131,13 +131,19 @@ pub async fn get_resource_miner(
         .cloned()
         .collect();
 
+    // non_special_event_types should exclude special activities.
+    let special_activity_set: FxHashSet<&String> = special_activity.iter().collect();
+    let mut non_special_event_types: Vec<String> = all_activities
+        .into_iter()
+        .filter(|activity| !special_activity_set.contains(activity))
+        .collect();
+
     // Converting sets to arrays for JSON response
     let mut object_resource: Vec<String> = object_resource.into_iter().collect();
     let mut object_type_not_resource: Vec<String> = object_type_not_resource.into_iter().collect();
-    let mut all_event_types = all_activities;
 
     // Sorting arrays for deterministic JSON response
-    all_event_types.sort();
+    non_special_event_types.sort();
     object_resource.sort();
     object_type_not_resource.sort();
     special_activity.sort();
@@ -152,7 +158,7 @@ pub async fn get_resource_miner(
     let response = ResourceMinerResponse {
         object_type_not_resource,
         object_resource,
-        all_event_types,
+        non_special_event_types,
         event_types_without_object_resource,
         object_not_resource_arcs,
         special_activity,
